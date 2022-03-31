473331ef80b91afe5b24d696d2936688cfb459c8
Grant Surlyn <grant@sunflowerseastar.com>
2022-03-31 15:21:21 -0400

    add grid creation functions/tests

--- src/minesweeper_clojure/core.cljs
+++ src/minesweeper_clojure/core.cljs
@@ -4,36 +4,57 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/minesweeper_clojure/core.cljs. Go ahead and edit it and see reloading in action.")
+(def dims (atom {:x-dim 4 :y-dim 4}))
+(def difficulty (atom 6)) ;; there will be 1 bomb per <difficulty> squares
 
-(defn multiply [a b] (* a b))
+(defn gen-bombs
+  "Create a vec of 'bombs', eg. [0 0 1 0] for a 2x2 grid with '4' difficulty (viz. '1 in 4')"
+  [x-dim y-dim difficulty]
+  (let [num-squares (* x-dim y-dim)
+        num-bombs (quot num-squares difficulty)]
+    (->> (concat (repeat num-bombs 1) (repeat (- num-squares num-bombs) 0))
+         shuffle)))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defn num-adjacent-bombs
+  "Given an index, bombs, and dims, determine the number of adjacent bombs."
+  [i bombs x-dim y-dim]
+  (let [is-left-edge (zero? (mod i x-dim))
+        is-right-edge (zero? (mod (inc i) x-dim))
+        is-top-edge (< i x-dim)
+        is-bottom-edge (>= i (- (* x-dim y-dim) x-dim))
+        left (if (not is-left-edge) (nth bombs (dec i)) 0)
+        right (if (not is-right-edge) (nth bombs (inc i)) 0)
+        top (if (not is-top-edge) (nth bombs (- i x-dim)) 0)
+        bottom (if (not is-bottom-edge) (nth bombs (+ i x-dim)) 0)
+        top-left (if (and (not is-left-edge) (not is-top-edge)) (nth bombs (- (dec i) x-dim)) 0)
+        top-right (if (and (not is-right-edge) (not is-top-edge)) (nth bombs (- (inc i) x-dim)) 0)
+        bottom-left (if (and (not is-left-edge) (not is-bottom-edge)) (nth bombs (+ (dec i) x-dim)) 0)
+        bottom-right (if (and (not is-right-edge) (not is-bottom-edge)) (nth bombs (+ (inc i) x-dim)) 0)
+        adjacent-bombs [top-left top top-right left right bottom-left bottom bottom-right]]
+    ;; (do (println [top-left top top-right])
+    ;;     (println [left (nth bs i) right])
+    ;;     (println [bottom-left bottom bottom-right]))
+    (reduce + adjacent-bombs)))
+
+(defn gen-board
+  "Create a vec of square maps, eg [{:i 0 :is-bomb true :is-revealed false :num-adjacent-bombs 1} ... ]"
+  [x-dim y-dim difficulty]
+  (let [bombs-vec (gen-bombs x-dim y-dim difficulty)]
+    (->> (map-indexed
+          (fn [i x] {:is-bomb x :num-adjacent-bombs (num-adjacent-bombs i bombs-vec x-dim y-dim) :is-revealed false})
+          bombs-vec)
+         vec)))
+
+(defn hello-world []
+  [:div "hi"])
 
 (defn get-app-element []
   (gdom/getElement "app"))
-
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/minesweeper_clojure/core.cljs and watch it change!"]])
-
 (defn mount [el]
   (rdom/render [hello-world] el))
-
 (defn mount-app-element []
   (when-let [el (get-app-element)]
     (mount el)))
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
-
-;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (mount-app-element))
--- test/minesweeper_clojure/core_test.cljs
+++ test/minesweeper_clojure/core_test.cljs
@@ -1,10 +1,16 @@
 (ns minesweeper-clojure.core-test
     (:require
      [cljs.test :refer-macros [deftest is testing]]
-     [minesweeper-clojure.core :refer [multiply]]))
+     [minesweeper-clojure.core :as core]))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
+(deftest gen-bombs-test
+  (is (= (count (core/gen-bombs 4 4 3)) 16)) ;; 4x4 grid -> 16 squares
+  (is (= (reduce + (core/gen-bombs 4 4 3)) 5))) ;; 1 bomb per 3 squares, 4x4 should have 5 bombs
 
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+(deftest num-adjacent-bombs-test
+  (is (= (core/num-adjacent-bombs 0 [0 0 0 0] 2 2) 0))
+  (is (= (core/num-adjacent-bombs 0 [0 1 1 1] 2 2) 3)))
+
+(deftest gen-board-test
+  (is (= (-> (core/gen-board 2 2 1) (nth 1) :is-bomb) 1))
+  (is (= (-> (core/gen-board 2 2 1) (nth 1) :num-adjacent-bombs) 3)))
