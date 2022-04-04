88fdb86050079628ce780bfcc1502faf89d1b3a8
Grant Surlyn <grant@sunflowerseastar.com>
2022-04-03 21:23:57 -0400

    recursively reveal blank/open squares

--- src/minesweeper_clojure/core.cljs
+++ src/minesweeper_clojure/core.cljs
@@ -15,46 +15,81 @@
     (->> (concat (repeat num-bombs 1) (repeat (- num-squares num-bombs) 0))
          shuffle)))
 
+(defn edges
+  "Given an index and dims, determine if/where it's on the board's border."
+  [i x-dim y-dim]
+  {:is-left-edge (zero? (mod i x-dim))
+   :is-right-edge (zero? (mod (inc i) x-dim))
+   :is-top-edge (< i x-dim)
+   :is-bottom-edge (>= i (- (* x-dim y-dim) x-dim))})
+
+(defn neighbor-indices
+  "Given an index and dims, return the immediate neighbor indices."
+  [i x-dim y-dim]
+  (let [{:keys [is-left-edge is-right-edge is-top-edge is-bottom-edge]} (edges i x-dim y-dim)]
+    {:left-i (if (not is-left-edge) (dec i) nil)
+     :right-i (if (not is-right-edge) (inc i) nil)
+     :top-i (if (not is-top-edge) (- i x-dim) nil)
+     :bottom-i (if (not is-bottom-edge) (+ i x-dim) nil)}))
+
+(defn adjacents
+  "Given an index, bombs, and dims, determine the number of adjacent bombs."
+  [i bombs x-dim y-dim]
+  (let [{:keys [is-left-edge is-right-edge is-top-edge is-bottom-edge]} (edges i x-dim y-dim)
+        {:keys [left-i right-i top-i bottom-i]} (neighbor-indices i x-dim y-dim)
+        left (if (not is-left-edge) (nth bombs left-i) nil)
+        right (if (not is-right-edge) (nth bombs right-i) nil)
+        top (if (not is-top-edge) (nth bombs top-i) nil)
+        bottom (if (not is-bottom-edge) (nth bombs bottom-i) nil)
+        top-left (if (and (not is-left-edge) (not is-top-edge)) (nth bombs (- (dec i) x-dim)) nil)
+        top-right (if (and (not is-right-edge) (not is-top-edge)) (nth bombs (- (inc i) x-dim)) nil)
+        bottom-left (if (and (not is-left-edge) (not is-bottom-edge)) (nth bombs (+ (dec i) x-dim)) nil)
+        bottom-right (if (and (not is-right-edge) (not is-bottom-edge)) (nth bombs (+ (inc i) x-dim)) nil)]
+    [top-left top top-right left right bottom-left bottom bottom-right]))
+
 (defn num-adjacent-bombs
   "Given an index, bombs, and dims, determine the number of adjacent bombs."
   [i bombs x-dim y-dim]
-  (let [is-left-edge (zero? (mod i x-dim))
-        is-right-edge (zero? (mod (inc i) x-dim))
-        is-top-edge (< i x-dim)
-        is-bottom-edge (>= i (- (* x-dim y-dim) x-dim))
-        left (if (not is-left-edge) (nth bombs (dec i)) 0)
-        right (if (not is-right-edge) (nth bombs (inc i)) 0)
-        top (if (not is-top-edge) (nth bombs (- i x-dim)) 0)
-        bottom (if (not is-bottom-edge) (nth bombs (+ i x-dim)) 0)
-        top-left (if (and (not is-left-edge) (not is-top-edge)) (nth bombs (- (dec i) x-dim)) 0)
-        top-right (if (and (not is-right-edge) (not is-top-edge)) (nth bombs (- (inc i) x-dim)) 0)
-        bottom-left (if (and (not is-left-edge) (not is-bottom-edge)) (nth bombs (+ (dec i) x-dim)) 0)
-        bottom-right (if (and (not is-right-edge) (not is-bottom-edge)) (nth bombs (+ (inc i) x-dim)) 0)
-        adjacent-bombs [top-left top top-right left right bottom-left bottom bottom-right]]
-    ;; (do (println [top-left top top-right])
-    ;;     (println [left (nth bs i) right])
-    ;;     (println [bottom-left bottom bottom-right]))
+  (let [adjacent-bombs (adjacents i bombs x-dim y-dim)]
     (reduce + adjacent-bombs)))
 
 (defn gen-board
   "Create a vec of square maps, eg [{:i 0 :is-bomb true :is-revealed false :num-adjacent-bombs 1} ... ]"
   [x-dim y-dim difficulty]
   (let [bombs-vec (gen-bombs x-dim y-dim difficulty)]
-    (->> (map-indexed
-          (fn [i x] {:is-bomb x :num-adjacent-bombs (num-adjacent-bombs i bombs-vec x-dim y-dim) :is-revealed false})
-          bombs-vec)
-         vec)))
+    (do
+      (println "bv: " bombs-vec)
+      (->> (map-indexed
+            (fn [i x] {:is-bomb x :num-adjacent-bombs (num-adjacent-bombs i bombs-vec x-dim y-dim) :is-revealed false})
+            bombs-vec)
+           vec))))
 
 ;; state
 
 (def has-initially-loaded (atom false))
-(def dims (atom {:x-dim 4 :y-dim 4}))
+(def dims (atom {:x-dim 10 :y-dim 10}))
 (def difficulty (atom 6)) ;; there will be 1 bomb per <difficulty> squares
 (def board (atom (gen-board (:x-dim @dims) (:y-dim @dims) @difficulty)))
 
 (defn reveal! [i]
-  (do
-    (swap! board assoc-in [i :is-revealed] true)))
+  (let [x-dim (:x-dim @dims) y-dim (:y-dim @dims)
+        {:keys [left-i right-i top-i bottom-i]} (neighbor-indices i x-dim y-dim)
+        {:keys [is-bomb num-adjacent-bombs]} (@board i)]
+    (do
+      (swap! board assoc-in [i :is-revealed] true)
+      (when (and (zero? is-bomb) (zero? num-adjacent-bombs))
+        (when (not (nil? left-i))
+          (let [{:keys [is-revealed is-bomb num-adjacent-bombs]} (@board left-i)]
+            (when (and (zero? is-bomb) (false? is-revealed) (zero? num-adjacent-bombs)) (reveal! left-i))))
+        (when (not (nil? right-i))
+          (let [{:keys [is-revealed is-bomb num-adjacent-bombs]} (@board right-i)]
+            (when (and (zero? is-bomb) (false? is-revealed) (zero? num-adjacent-bombs)) (reveal! right-i))))
+        (when (not (nil? top-i))
+          (let [{:keys [is-revealed is-bomb num-adjacent-bombs]} (@board top-i)]
+            (when (and (zero? is-bomb) (false? is-revealed) (zero? num-adjacent-bombs)) (reveal! top-i))))
+        (when (not (nil? bottom-i))
+          (let [{:keys [is-revealed is-bomb num-adjacent-bombs]} (@board bottom-i)]
+            (when (and (zero? is-bomb) (false? is-revealed) (zero? num-adjacent-bombs)) (reveal! bottom-i))))))))
 
 (defn main []
   (create-class
--- test/minesweeper_clojure/core_test.cljs
+++ test/minesweeper_clojure/core_test.cljs
@@ -1,15 +1,23 @@
 (ns minesweeper-clojure.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [minesweeper-clojure.core :as core]))
+  (:require
+   [cljs.test :refer-macros [deftest is testing]]
+   [minesweeper-clojure.core :as core]))
 
 (deftest gen-bombs-test
   (is (= (count (core/gen-bombs 4 4 3)) 16)) ;; 4x4 grid -> 16 squares
   (is (= (reduce + (core/gen-bombs 4 4 3)) 5))) ;; 1 bomb per 3 squares, 4x4 should have 5 bombs
 
+(deftest edges-test
+  (is (= (core/edges 0 2 2)) {:is-left-edge true, :is-right-edge false, :is-top-edge true, :is-bottom-edge false})
+  (is (= (core/edges 5 4 4)) {:is-left-edge false, :is-right-edge false, :is-top-edge false, :is-bottom-edge false}))
+
 (deftest num-adjacent-bombs-test
   (is (= (core/num-adjacent-bombs 0 [0 0 0 0] 2 2) 0))
   (is (= (core/num-adjacent-bombs 0 [0 1 1 1] 2 2) 3)))
+
+(deftest num-adjacents-test
+  (is (= (core/adjacents 0 [0 0 0 0] 2 2) [nil nil nil nil 0 nil 0 0]))
+  (is (= (core/adjacents 0 [0 1 1 1] 2 2) [nil nil nil nil 1 nil 1 1])))
 
 (deftest gen-board-test
   (is (= (-> (core/gen-board 2 2 1) (nth 1) :is-bomb) 1))
